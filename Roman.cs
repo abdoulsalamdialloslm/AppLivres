feab1fdddf2757dab5603648d6056fff66693cce
abdoul-salam-diallo <abdoulsalam.diallo@monboreal.ca>
2024-10-17 19:46:38 -0400

    ajout de La class Roman

---
+++ Roman.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppConsoleLivres
+{
+    internal class Roman : Livre
+    {
+        public string Genre { get; set; }
+
+        public Roman(string titre, string auteur, string genre, int nombredepages)
+            : base(titre, auteur, nombredepages)
+        {
+            Genre = genre;
+        }
+
+
+        public override void AfficherDetails()
+        {
+            base.AfficherDetails();
+            Console.WriteLine($"Genre: {Genre}");
+        }
+    }
+}
