920661d3d87347107fed20201e8b1c2be977999f
abdoul-salam-diallo <abdoulsalam.diallo@monboreal.ca>
2024-10-17 19:22:21 -0400

    ajout de la class revue

---
+++ Revue.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppConsoleLivres
+{
+    internal class Revue : Livre
+    
+        {
+            public int Annee { get; set; }
+            public int Numero { get; set; }
+
+            public Revue(string titre, string auteur, int nombrepages, int annee, int numero)
+                : base(titre, auteur, nombrepages)
+            {
+                Annee = annee;
+                Numero = numero;
+            }
+
+            public override void AfficherDetails()
+            {
+                base.AfficherDetails();
+                Console.WriteLine($"Annee: {Annee}, Numéro: {Numero}");
+            }
+        }
+
+    
+}
