4fd8bda7bbaec3d25e6b4d113e6b05979ec581d1
abdoul-salam-diallo <abdoulsalam.diallo@monboreal.ca>
2024-10-17 18:47:01 -0400

    class livre ajouter

---
+++ Livre.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppConsoleLivres
+{
+    internal class Livre
+    {
+        public string Titre { get; set; }
+        public string Auteur { get; set; }
+        public int NombrePages { get; set; }
+
+        public Livre(string titre, string auteur, int nombrepages)
+        {
+            Titre = titre;
+            Auteur = auteur;
+            NombrePages = nombrepages;
+        }
+
+
+        public virtual void AfficherDetails()
+        {
+            Console.WriteLine($"Titre: {Titre}, Auteur: {Auteur}, NombrePages: {NombrePages}");
+
+
+        }
+
+    }
+}
--- Program.cs
+++ Program.cs
@@ -10,6 +10,7 @@ namespace AppConsoleLivres
     {
         static void Main(string[] args)
         {
+
         }
     }
 }
